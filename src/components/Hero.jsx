2e6fe873f7bb75c17b34e074af09a8773a5c4c2a
thetharz <tharinduimalka915@gmail.com>
2023-09-17 19:25:12 +0530

    speed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
             typeSpeed={120}
             backSpeed={140}
             loop
-            //typing speed animation this function
+            //typing speed animation this function change to speed
           />
         </div>
         <p className='md:text-2xl text-xl font-bold text-gray-500'>
