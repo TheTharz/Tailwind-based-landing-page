a71f536b56c9855cc817bdcc9957c4fa9791b810
thetharz <tharinduimalka915@gmail.com>
2023-09-20 19:41:06 +0530

    daily

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
+import React from 'react';
 
+//newsletter component goes here
 const NewsLetter = () => {
   return (
     <div className='w-full py-16 text-white px-4'>
-        <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
-            <div className='lg:col-span-2 my-4'>
-                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold'>Want tips & trick to optimize your flow?</h1>
-                <p>Sign up to our newsletter and stay up to date.</p>
-            </div>
-            <div className='my-4'>
-                <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
-                    <input className='p-3 flex w-full rounded-md text-black' type="email" placeholder='Enter Email'/>
-                    <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 px-6 py-3 text-black ml-4'>Notify Me</button>
-                </div>
-                <p>
-                    We care about the protection of your data.Read our <span className='text-[#00df9a]'>Privacy Policy.</span>
-                </p>
-            </div>
+      <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
+        <div className='lg:col-span-2 my-4'>
+          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold'>
+            Want tips & trick to optimize your flow?
+          </h1>
+          <p>Sign up to our newsletter and stay up to date.</p>
         </div>
+        <div className='my-4'>
+          <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
+            <input
+              className='p-3 flex w-full rounded-md text-black'
+              type='email'
+              placeholder='Enter Email'
+            />
+            <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 px-6 py-3 text-black ml-4'>
+              Notify Me
+            </button>
+          </div>
+          <p>
+            We care about the protection of your data.Read our{' '}
+            <span className='text-[#00df9a]'>Privacy Policy.</span>
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewsLetter
+export default NewsLetter;
